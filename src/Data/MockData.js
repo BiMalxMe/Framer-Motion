42c654265aac5c70b7c0ec7fe83a24665a70fe5c
BiMalxMe <journeyxbimal@gmail.com>
2024-11-21 13:54:20 +0545

    'framermotionBasica'

---
+++ src/Data/MockData.js
@@ -0,0 +1,60 @@
+import Img1 from "../assets/1.png"
+import Img2 from "../assets/2.png"
+import Img3 from "../assets/3.png"
+
+export const NavbarData = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "#",
+  },
+];
+
+export const HeadphoneDatta = [
+  {
+    id: 1,
+    image: Img1,
+    title: "Apple Airpords Wireless Over-Ear Headphones",
+    subtitle:
+      "AirPods might make random noises if they are experiencing interference,have software glitches are far from the connected device, or have software glitches",
+    price:'$40.99',
+    model:'Airpords',
+    bgcolor:'#e85950',
+    },
+  {
+    id: 2,
+    image: Img2,
+    title: "Apple Airpords Pro Wireless Over-Ear Headphones ",
+    subtitle:
+      "AirPods might make random noises if they are experiencing interference,have software glitches are far from the connected device, or have software glitches",
+    price:'$100.00',
+    model:'Max',
+    bgcolor:'#24b4bc',
+    },
+  {
+    id: 3,
+    image: Img3,
+    title: "Apple Airpords Wireless Over-Ear Headphones",
+    subtitle:
+      "AirPods might make random noises if they are experiencing interference,have software glitches are far from the connected device, or have software glitches",
+    price:'$100.00',
+    model:'Pro',
+    bgcolor:'#7aa10e',
+    }
+
+];
